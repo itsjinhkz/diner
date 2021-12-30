5fca41b8d841c4575c05e31447d0579f75db0832
itsjinhk <darkjinh@gmail.com>
2021-12-30 15:17:59 +0700

    add news part

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
+// back-to-top
+var btn = $("#back-to-top");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
